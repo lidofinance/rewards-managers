714c2f5fe0b7c04abf3f1cde6c5fcac8c7d95015
Victor Suzdalev <victor.suzdalev@gmail.com>
2021-02-26 23:26:12 +0300

    chore: add ARCx contracts

---
+++ contracts/arcx_contracts/token/ArcxToken.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.16;
+
+import {Ownable} from "../lib/Ownable.sol";
+
+import {IMintableToken} from "../token/IMintableToken.sol";
+
+import {BaseERC20} from "./BaseERC20.sol";
+
+contract ArcxToken is BaseERC20, IMintableToken, Ownable {
+
+    // ============ Constructor ============
+
+    constructor()
+        public
+        BaseERC20("ARC Governance Token", "ARCX", 18)
+    { }
+
+    // ============ Core Functions ============
+
+    function mint(
+        address to,
+        uint256 value
+    )
+        external
+        onlyOwner
+    {
+        _mint(to, value);
+    }
+
+    function burn(
+        address to,
+        uint256 value
+    )
+        external
+        onlyOwner
+    {
+        _burn(to, value);
+    }
+
+}
