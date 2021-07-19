aa8664c15a217a738aaacad9e8e382ddd5f28d91
Alexandr Tarelkin <tarelkinaa@bk.ru>
2021-07-19 16:01:52 +0300

    tests: remove unused

--- tests/test_end_of_funding_date.py
+++ tests/test_end_of_funding_date.py
@@ -52,32 +52,3 @@ def test_out_of_funding_date_with_limit_change(
     rewards_manager.change_rewards_limit(2 * rewards_limit, {"from": ldo_agent})
     assert rewards_manager.rewards_limit_per_period() == 2 * rewards_limit
     assert rewards_manager.out_of_funding_date() == out_of_funding_date
-
-
-def test_out_of_funding_date_with_limit_change(
-    rewards_manager, 
-    ldo_token, 
-    dao_treasury,
-    ldo_agent
-):
-    amount = 100000 * 10**18
-    
-    ldo_token.transfer(rewards_manager, amount, {"from": dao_treasury})
-    assert ldo_token.balanceOf(rewards_manager) == amount
-
-    out_of_funding_date = rewards_manager.tx.timestamp + rewards_period * 4
-
-    chain.sleep(rewards_period)
-    chain.mine()
-    assert rewards_manager.out_of_funding_date() == out_of_funding_date
-    assert rewards_manager.available_allocations() == rewards_limit
- 
-    out_of_funding_date = rewards_manager.tx.timestamp + 2 * rewards_period
-
-    rewards_manager.change_rewards_limit(2 * rewards_limit, {"from": ldo_agent})
-    assert rewards_manager.rewards_limit_per_period() == 2 * rewards_limit
-    assert rewards_manager.out_of_funding_date() == out_of_funding_date
-
-
-
-    
