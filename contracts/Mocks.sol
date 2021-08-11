a46a30b3392d49256613b7f50c44327e7fb416b7
Pavel Isayenko <pavel.isayenko@gmail.com>
2021-08-11 23:50:43 +0600

    Tests for is_reward_period_finished(), out_of_funding_date()

--- contracts/Mocks.sol
+++ contracts/Mocks.sol
@@ -11,17 +11,20 @@ contract StubERC20 is ERC20 {
 }
 
 contract StubFarmingRewards {
+    uint public periodFinish;
+
     uint private _tokenRewards;
     uint private _giftIndex;
 
-    constructor(uint tokenRewards_, uint giftIndex_) public {
+    constructor(uint tokenRewards_, uint giftIndex_, uint periodFinish_) public {
         _tokenRewards = tokenRewards_;
         _giftIndex = giftIndex_;
+        periodFinish = periodFinish_;
     }
 
     function notifyRewardAmount(uint, uint256) public {}
     
     function tokenRewards(uint) public view returns(address, uint256, uint256, address, uint256, uint256, uint256, uint256) {
-        return (address(0), 1, 1, address(0), 1, 1, 1, 1);
+        return (address(0), 1, 1, address(0), periodFinish, 1, 1, 1);
     }
 }
--- test/rewards_manager.spec.js
+++ test/rewards_manager.spec.js
@@ -3,11 +3,18 @@ const StubFarmingRewards = artifacts.require("StubFarmingRewards");
 const RewardsManager = artifacts.require("RewardsManager");
 const truffleAssert = require('truffle-assertions');
 const zero_address = "0x0000000000000000000000000000000000000000";
+const timeInThePast = 1628687598;
+
+
+function debug(name, val) {
+    console.log(name, val, typeof(val));
+}
 
 contract('RewardsManager', async (accounts) => {
     const [contractsOwner, otherAccount] = accounts;
     const tokenRewards = 100;
     const giftIndex = 1;
+
 
     let token;
     let rewards;
@@ -15,13 +22,9 @@ contract('RewardsManager', async (accounts) => {
 
     beforeEach(async () => {
         token = await StubERC20.new("Lido DAO Token", "LDO", 250000, { from: contractsOwner });
-        rewards = await StubFarmingRewards.new(tokenRewards, giftIndex, { from: contractsOwner });
+        rewards = await StubFarmingRewards.new(tokenRewards, giftIndex, timeInThePast, { from: contractsOwner });
         rewardsManager = await RewardsManager.new(giftIndex, rewards.address, token.address, { from: contractsOwner });
     });
-
-    // it('Rewards contract address not null reverted', async () => {
-    //     truffleAssert.fails(RewardsManager.new(giftIndex, zero_address, token.address), { from: contractsOwner });
-    // });
 
     it('Constructor: Public varialbles set', async () => {
         assert.equal(await rewardsManager.giftIndex(), giftIndex, "giftIndex should match");
@@ -29,18 +32,45 @@ contract('RewardsManager', async (accounts) => {
         assert.equal(await rewardsManager.rewardToken(), token.address, "token address should match");
     });
 
+    it('out_of_funding_date', async () => {
+        const result = (await rewardsManager.out_of_funding_date()).toNumber();
+        assert.equal(result, timeInThePast, "out_of_funding_date should match");
+    });
 
-    it('Owner can set rewards contract address (setRewardsContract)', async () => {
+    it('is_reward_period_finished: in the past', async () => {
+        const currentBlockTimestamp = (await web3.eth.getBlock(await web3.eth.getBlockNumber())).timestamp;
+        const periodFinish = (await rewards.periodFinish()).toNumber();
+        const is_reward_period_finished = await rewardsManager.is_reward_period_finished();
+
+        assert.equal(is_reward_period_finished, currentBlockTimestamp >= periodFinish, "currentBlockTimestamp is less than periodFinish");
+    });
+
+    it('is_reward_period_finished: in the future', async () => {
+        const currentBlockTimestamp = (await web3.eth.getBlock(await web3.eth.getBlockNumber())).timestamp;
+        const timeInTheFuture = currentBlockTimestamp + 1314000;
+
+        rewards = await StubFarmingRewards.new(tokenRewards, giftIndex, timeInTheFuture, { from: contractsOwner });
+        rewardsManager = await RewardsManager.new(giftIndex, rewards.address, token.address, { from: contractsOwner });
+
+        const periodFinish = (await rewards.periodFinish()).toNumber();
+        const is_reward_period_finished = await rewardsManager.is_reward_period_finished();
+
+        assert.equal(is_reward_period_finished, currentBlockTimestamp >= periodFinish, "currentBlockTimestamp is more than or equal to periodFinish");
+    });
+
+    it('setRewardsContract: Owner can set rewards contract address', async () => {
         const newAddr = "0xa355B4B904ce09Bd1847f4cf133769BC0dfBC51B";
 
         truffleAssert.passes(rewardsManager.setRewardsContract(newAddr), { from: contractsOwner });
+        assert.equal((await rewardsManager.rewardsContract()).valueOf(), rewards.address, "rewardsContract address should match");
     });
 
-    it('Other account cannot set rewards contract address (setRewardsContract)', async () => {
+    it('setRewardsContract: Other account cannot set rewards contract address', async () => {
         const newAddr = "0xa355B4B904ce09Bd1847f4cf133769BC0dfBC51B";
 
         truffleAssert.fails(rewardsManager.setRewardsContract(newAddr), { from: otherAccount });
     });
+
 });
 
 // !!!! WIP
