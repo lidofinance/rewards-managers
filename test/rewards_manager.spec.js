c33af4a0ff98567aa7cc41c7bc6b0f7cb6ae0d6e
Pavel Isayenko <pavel.isayenko@gmail.com>
2021-08-12 15:15:50 +0600

    Tests for start_next_rewards_period(), use local variables for additional instances.

--- test/rewards_manager.spec.js
+++ test/rewards_manager.spec.js
@@ -37,9 +37,9 @@ contract('RewardsManager', async (accounts) => {
     });
 
     it('out_of_funding_date', async () => {
-        const result = (await rewardsManager.out_of_funding_date()).toNumber();
+        const outOfFundingDate = (await rewardsManager.out_of_funding_date()).toNumber();
 
-        assert.equal(result, timeInThePast, "out_of_funding_date should match");
+        assert.equal(outOfFundingDate, timeInThePast, "out_of_funding_date should match");
     });
 
     it('is_reward_period_finished: in the past', async () => {
@@ -54,13 +54,40 @@ contract('RewardsManager', async (accounts) => {
         const currentBlockTimestamp = (await web3.eth.getBlock(await web3.eth.getBlockNumber())).timestamp;
         const timeInTheFuture = currentBlockTimestamp + 1314000;
 
-        rewards = await StubFarmingRewards.new(tokenRewards, giftIndex, timeInTheFuture, { from: contractsOwner });
-        rewardsManager = await RewardsManager.new(giftIndex, rewards.address, token.address, { from: contractsOwner });
+        const rewardsLocal = await StubFarmingRewards.new(tokenRewards, giftIndex, timeInTheFuture, { from: contractsOwner });
+        const rewardsManagerLocal = await RewardsManager.new(giftIndex, rewardsLocal.address, token.address, { from: contractsOwner });
 
-        const periodFinish = (await rewards.periodFinish()).toNumber();
-        const isRewardPeriodFinished = await rewardsManager.is_reward_period_finished();
+        const periodFinish = (await rewardsLocal.periodFinish()).toNumber();
+        const isRewardPeriodFinished = await rewardsManagerLocal.is_reward_period_finished();
 
         assert.equal(isRewardPeriodFinished, currentBlockTimestamp >= periodFinish, "currentBlockTimestamp is more than or equal to periodFinish");
+    });
+
+    it('start_next_rewards_period: token amount = 0', async () => {
+        await truffleAssert.fails(rewardsManager.start_next_rewards_period({ from: contractsOwner }), truffleAssert.ErrorType.REVERT, "Rewards disabled");
+    });
+
+    it('start_next_rewards_period: token amount > 0, rewards period not finished', async () => {
+        const currentBlockTimestamp = (await web3.eth.getBlock(await web3.eth.getBlockNumber())).timestamp;
+        const timeInTheFuture = currentBlockTimestamp + 1314000;
+
+        const tokenLocal = await StubERC20.new("Lido DAO Token", "LDO", 250000, { from: contractsOwner });
+        const rewardsLocal = await StubFarmingRewards.new(tokenRewards, giftIndex, timeInTheFuture, { from: contractsOwner });
+        const rewardsManagerLocal = await RewardsManager.new(giftIndex, rewardsLocal.address, tokenLocal.address, { from: contractsOwner });
+
+        await tokenLocal.transfer(rewardsManagerLocal.address, tokenRewards, { from: contractsOwner });
+
+        await truffleAssert.fails(rewardsManagerLocal.start_next_rewards_period({ from: contractsOwner }), truffleAssert.ErrorType.REVERT, "Rewards period not finished");
+    });
+
+    it('start_next_rewards_period: token amount > 0, unable to transfer tokens', async () => {
+        await token.transfer(rewardsManager.address, tokenRewards, { from: contractsOwner });
+
+        await truffleAssert.fails(rewardsManager.start_next_rewards_period({ from: contractsOwner, gas: 21000 }));
+    });
+
+    it('start_next_rewards_period: passes', async () => {
+        await truffleAssert.passes(rewardsManager.start_next_rewards_period({ from: contractsOwner }));
     });
 
     it('setRewardsContract: owner can set rewards contract address', async () => {
