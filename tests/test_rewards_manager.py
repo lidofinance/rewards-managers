1785243f65a18654df1808c610d71817fd79527c
Pavel Isayenko <pavel.isayenko@gmail.com>
2021-08-31 17:20:53 +0600

    Fixed test_owner_recovers_erc20_to_own_address(), test_ownership_can_be_transferred()

--- tests/test_rewards_manager.py
+++ tests/test_rewards_manager.py
@@ -18,11 +18,13 @@ def test_stranger_cannot_transfer_ownership(rewards_manager, stranger):
 
 
 def test_ownership_can_be_transferred(rewards_manager, ape, stranger):
+    assert rewards_manager.owner() != stranger
+    
     tx = rewards_manager.transfer_ownership(stranger, {"from": ape})
 
     assert rewards_manager.owner() == stranger
     assert len(tx.events) == 1
-    assert tx.events[0]['new_owner'] == stranger
+    assert tx.events['OwnershipTransferred']['new_owner'] == stranger
 
 
 def test_ownership_can_be_transferred_to_zero_address(rewards_manager, ape):
@@ -96,6 +98,12 @@ def test_stranger_cannot_recover_erc20(rewards_manager, ldo_token, stranger):
 
 def test_owner_recovers_erc20_to_own_address(rewards_manager, ldo_token, ape):
     assert ldo_token.balanceOf(rewards_manager) == 0
+
+    ldo_token.transfer(rewards_manager, rewards_amount, {"from": accounts.at(lido_dao_agent_address, force=True)})
+    assert ldo_token.balanceOf(rewards_manager) == rewards_amount
+
+    rewards_manager.recover_erc20(ldo_token, rewards_amount, ape, {"from": ape})
+    assert ldo_token.balanceOf(ape) == rewards_amount
 
 
 def test_owner_recovers_erc20_to_stranger_address(rewards_manager, ldo_token, ape, stranger):
